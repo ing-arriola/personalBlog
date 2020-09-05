febfc84c0e05c7897ca5008b08c924222cb3d530
ing-arriola <ingeniero.arriola@gmail.com>
2020-09-05 12:31:51 -0600

    Added folder structure, home component and styles for home text text-box

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
-  return <div className="App">HOLAS</div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mainImage">
+      <div class="text-box">
+        <h1 class="primary-text">
+          <span class="primary-text-main">Jaime Arriola</span>
+          <span class="primary-text-sub">JavaScript Developer</span>
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
