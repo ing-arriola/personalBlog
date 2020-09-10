6e535b601a075e3879091ed7c9050a587591c2d1
ing-arriola <ingeniero.arriola@gmail.com>
2020-09-10 16:13:56 -0600

    Improved about description, Added styes for about page and added my photo to the about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,20 +1,28 @@
 import React from "react";
+import jarriolaPhoto from "../../images/jaimeArriola.png";
 
 const About = () => {
   return (
-    <div>
-      <h2>Who am I?</h2>
-      <p>
-        I’m a FrontEnd Developer who enjoys creating intuitive, useful and
-        organized web applications to help people do their job in an effective
-        way.
-      </p>
-      <h2>What am I looking for?</h2>
-      <p>
-        I’m looking for a company which gives its employees chances to do remote
-        jobs, propose innovative ideas, growth opportunities on learning and
-        with better positions based on performance.
-      </p>
+    <div id="about-container">
+      <div id="about-description">
+        <h2>Who am I?</h2>
+        <p>
+          I’m a FrontEnd Developer who enjoys creating intuitive, useful and
+          organized web applications to help people do their job in an effective
+          way.
+        </p>
+        <h2>What am I looking for?</h2>
+        <p>I’m looking for a remote job only which allows me:</p>
+        <ul>
+          <li>Get out from my confor zone</li>
+          <li>Propose innovative ideas</li>
+          <li>Learn new technologies</li>
+          <li>Growth Opportunities</li>
+        </ul>
+      </div>
+      <div id="about-photo">
+        <img src={jarriolaPhoto} className="" alt="JarriolaPhoto" />
+      </div>
     </div>
   );
 };
