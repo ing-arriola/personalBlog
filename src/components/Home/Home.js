868d985ba4a783c39297ccce4d59d62a64bce105
ing-arriola <ingeniero.arriola@gmail.com>
2020-09-12 11:30:06 -0600

    applied BEM on names of HOME elements styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,13 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 const Home = () => {
   return (
-    <div className="mainImage">
-      <div className="text-box">
-        <h1 className="primary-text">
-          <span className="primary-text-main">Jaime Arriola</span>
-          <span className="primary-text-sub">JavaScript Developer</span>
+    <div className="header">
+      <div className="header__text-box">
+        <h1 className="header__primary-text">
+          <span className="heading-primary--main">Jaime Arriola</span>
+          <span className="heading-primary--sub">JavaScript Developer</span>
         </h1>
-        <NavLink to="/work" className="btn btn-white btn-animated-from-bottom">
+        <NavLink
+          to="/work"
+          className="btn btn--white btn--animated-from-bottom"
+        >
           Check my work
         </NavLink>
       </div>
