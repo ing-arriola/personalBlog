b5d778977e9b525c72720c3f63a4d6222efaca10
ing-arriola <ingeniero.arriola@gmail.com>
2020-09-24 18:10:03 -0600

    Added name effect on About component, also deleted no necesary effects on home component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,56 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import jarriolaPhoto from "../../images/jaimeArriola.png";
 import * as logos from "../../images/about/index";
 import { motion } from "framer-motion";
 const About = () => {
+  const [visible, setVisible] = useState(true);
+
+  setTimeout(() => {
+    setVisible(false);
+  }, 1200);
+
+  const effectName = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
   return (
     <div>
-
-      
+      {visible ? (
+        <div className="contact-container big-name ">
+          <motion.p
+            variants={effectName}
+            initial="visible"
+            animate="hidden"
+            transition={{ delay: 1 }}
+          >
+            Jaime Arriola
+          </motion.p>
+        </div>
+      ) : (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          className="about-container"
+        >
+          <div id="about-container__info-section">
+            <div id="about-photo" className="info-section__column">
+              <img src={jarriolaPhoto} className="" alt="JarriolaPhoto" />
+            </div>
+            <div id="about-description" className="info-section__column">
+              <h2>Who am I?</h2>
+              <p>
+                I’m a FrontEnd Developer who enjoys creating intuitive, useful
+                and organized web applications to help people do their job in an
+                effective way.
+              </p>
+              <h2>What am I looking for?</h2>
+              <p>I’m looking for a remote job only which allows me:</p>
+              <ul>
+                <li>Get out from my confort zone</li>
+                <li>Propose innovative ideas</li>
+                <li>Learn new technologies... because I love keep learning!</li>
+                <li>Growth Opportunities</li>
+              </ul>
+            </div>
+          </div>
+          <div id="about-container__skills-section">
+            <div id="skills-section__title">
+              <h2>My Dev skills</h2>
+            </div>
+            <div id="skills--section__logos">
+              <img
+                className="logos__logo"
+                src={logos.jsLogo}
+                alt="JavaScript"
+              />
+              <img className="logos__logo" src={logos.reactLogo} alt="React" />
+              <img className="logos__logo" src={logos.nodeLogo} alt="NodeJS" />
+              <img className="logos__logo" src={logos.html} alt="HTML" />
+              <img className="logos__logo" src={logos.cssLogo} alt="CSS" />
+              <img className="logos__logo" src={logos.sassLogo} alt="SASS" />
+              <img
+                className="logos__logo"
+                src={logos.gatsbyLogo}
+                alt="GATSBY"
+              />
+              <img
+                className="logos__logo"
+                src={logos.mongoLogo}
+                alt="MONGODB"
+              />
+              <img
+                className="logos__logo"
+                src={logos.moodleLogo}
+                alt="MOODLE"
+              />
+            </div>
+          </div>
+        </motion.div>
+      )}
     </div>
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      className="about-container"
-    >
-      <div id="about-container__info-section">
-        <div id="about-photo" className="info-section__column">
-          <img src={jarriolaPhoto} className="" alt="JarriolaPhoto" />
-        </div>
-        <div id="about-description" className="info-section__column">
-          <h2>Who am I?</h2>
-          <p>
-            I’m a FrontEnd Developer who enjoys creating intuitive, useful and
-            organized web applications to help people do their job in an
-            effective way.
-          </p>
-          <h2>What am I looking for?</h2>
-          <p>I’m looking for a remote job only which allows me:</p>
-          <ul>
-            <li>Get out from my confort zone</li>
-            <li>Propose innovative ideas</li>
-            <li>Learn new technologies... because I love keep learning!</li>
-            <li>Growth Opportunities</li>
-          </ul>
-        </div>
-      </div>
-      <div id="about-container__skills-section">
-        <div id="skills-section__title">
-          <h2>My Dev skills</h2>
-        </div>
-        <div id="skills--section__logos">
-          <img className="logos__logo" src={logos.jsLogo} alt="JavaScript" />
-          <img className="logos__logo" src={logos.reactLogo} alt="React" />
-          <img className="logos__logo" src={logos.nodeLogo} alt="NodeJS" />
-          <img className="logos__logo" src={logos.html} alt="HTML" />
-          <img className="logos__logo" src={logos.cssLogo} alt="CSS" />
-          <img className="logos__logo" src={logos.sassLogo} alt="SASS" />
-          <img className="logos__logo" src={logos.gatsbyLogo} alt="GATSBY" />
-          <img className="logos__logo" src={logos.mongoLogo} alt="MONGODB" />
-          <img className="logos__logo" src={logos.moodleLogo} alt="MOODLE" />
-        </div>
-      </div>
-    </motion.div>
   );
 };
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,40 +3,15 @@ import { NavLink } from "react-router-dom";
 import { motion } from "framer-motion";
 
 const Home = () => {
-  const [visible, setbolado] = useState(true);
-
-  const effect1 = {
-    visible: { opacity: 1 },
-    hidden: { opacity: 0 },
-  };
-
   return (
     <div className="header">
       <div className="header__text-box">
         <h1 className="header__primary-text">
           <span className="heading-primary--main">Jaime Arriola</span>
-          {visible ? (
-            <motion.span
-              variants={effect1}
-              initial="visible"
-              animate="hidden"
-              exit={{ x: 200 }}
-              transition={{ duration: 1, yoyo: Infinity }}
-              className="heading-primary--sub"
-            >
-              Always Learning, always growing
-            </motion.span>
-          ) : (
-            <motion.span
-              variants={effect1}
-              initial="visible"
-              animate="hidden"
-              transition={{ duration: 1, yoyo: Infinity }}
-              className="heading-primary--sub"
-            >
-              Building your ideas into your perfect App
-            </motion.span>
-          )}
+
+          <motion.span className="heading-primary--sub">
+            Turning your ideas into your perfect website
+          </motion.span>
         </h1>
         <NavLink
           to="/work"
