611204f06dabd65de30216d45709eef185586490
ing-arriola <ingeniero.arriola@gmail.com>
2020-10-18 08:40:09 -0600

    Added links and styles to footer

--- src/components/Navigation/Footer.js
+++ src/components/Navigation/Footer.js
@@ -1,19 +1,42 @@
 import React from "react";
-import { FaTwitter, FaLinkedin, FaYoutube } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import Logo from "./Logo";
+import { FaGithub, FaTwitter, FaLinkedin, FaYoutube } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <div className="main-footer">
-      <div className="main-footer__personal-brand">Jaime Arriola 2020</div>
+      <div className="main-footer__personal-brand">
+        <Logo color={true} />
+        Jaime Arriola 2020
+      </div>
       <div className="main-footer__social">
-        <a href="#">
-          <FaTwitter />
+        <a
+          href="https://github.com/ing-arriola"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <FaGithub size={25} />
         </a>
-        <a href="#">
-          <FaLinkedin />
+        <a
+          href="https://twitter.com/JaimeArriola8"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <FaTwitter size={25} />
         </a>
-        <a href="#">
-          <FaYoutube />
+        <a
+          href="https://www.linkedin.com/in/jaime-arriola-1457bb1b2"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <FaLinkedin size={25} />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCTMHspxBI1kIxqIL0cVattA"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <FaYoutube size={25} />
         </a>
       </div>
     </div>
