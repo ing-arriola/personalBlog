7a0745385420c79c525b8641a358cbe6cdb23419
ing-arriola <ingeniero.arriola@gmail.com>
2020-10-18 20:00:54 -0600

    Added new info-section on home, also added styles on info-section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,44 @@
 import React from "react";
+import infoPicture from "../../images/section-info.jpg";
 import { NavLink } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="header">
-      <div className="header__text-box">
-        <h1 className="header__primary-text">
-          <span className="heading-primary--main">Jaime Arriola</span>
+    <div className="home-container">
+      <div className="header">
+        <div className="header__text-box">
+          <h1 className="header__primary-text">
+            <span className="heading-primary--main">Jaime Arriola</span>
 
-          <span className="heading-primary--sub">
-            Turning your ideas into your perfect website
-          </span>
-        </h1>
-        <NavLink
-          to="/work"
-          className="btn btn--white btn--animated-from-bottom"
-        >
-          Check my work
-        </NavLink>
+            <span className="heading-primary--sub">
+              Turning your ideas into your perfect website
+            </span>
+          </h1>
+          <NavLink
+            to="/work"
+            className="btn btn--white btn--animated-from-bottom"
+          >
+            Check my work
+          </NavLink>
+        </div>
       </div>
+      <section className="info-section">
+        <div className="info-section--title">
+          <p>Build your perfect web application will be my pleasure</p>
+        </div>
+        <div className="info-section--data">
+          <div className="data--picture">
+            <img src={infoPicture} className="" alt="infoPicture" />
+          </div>
+          <div className="data--text">
+            <p>
+              If you want to improve some process on your bussiness by using a
+              customized web app or just want a website that make your bussiness
+              stand out, I'll be happy to make that happen{" "}
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
