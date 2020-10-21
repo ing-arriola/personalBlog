1e1af67a207a3320ec0fbbeb1b8f1ec696050bc7
ing-arriola <ingeniero.arriola@gmail.com>
2020-10-21 05:21:43 -0600

    another test

---
+++ build/precache-manifest.6f1f27312759d026f5717488fc8f19e3.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb3f4dbbba406d11d3f527cc71ddac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53dd1d03cca9c3bb551c",
+    "url": "/static/css/main.375996b1.chunk.css"
+  },
+  {
+    "revision": "41e3d51ef9f88f91f4f1",
+    "url": "/static/js/2.14464438.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.14464438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53dd1d03cca9c3bb551c",
+    "url": "/static/js/main.ea02b1b6.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "2aa1cb6f3c44c24e7eb4c8a025fbf883",
+    "url": "/static/media/burgerBuilder.2aa1cb6f.png"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/static/media/css-3.d3ad67fa.svg"
+  },
+  {
+    "revision": "6962bac836817bbc79be27b3d95c9e33",
+    "url": "/static/media/frontPage.6962bac8.jpg"
+  },
+  {
+    "revision": "aec012ae2ee1860442d37e9c7c274c57",
+    "url": "/static/media/gatsby.aec012ae.svg"
+  },
+  {
+    "revision": "30f6a60289be51e5e2af5be07caf453e",
+    "url": "/static/media/githubFinder.30f6a602.png"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/static/media/html-5.24845c5c.svg"
+  },
+  {
+    "revision": "329a6897f293e2c25821c601e979e118",
+    "url": "/static/media/htmlUdemy.329a6897.pdf"
+  },
+  {
+    "revision": "61be435f6fb80c84ac94870cb5ed7b88",
+    "url": "/static/media/jaimeArriola.61be435f.png"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "04601a7fcbf8b42de063a2ff86020bc8",
+    "url": "/static/media/matter.04601a7f.jpg"
+  },
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/static/media/mongodb.2aa7f7b5.svg"
+  },
+  {
+    "revision": "7d5a2f411f778902b2099d7386e173bb",
+    "url": "/static/media/moodle.7d5a2f41.svg"
+  },
+  {
+    "revision": "5f301abfa03a58c614a6bf60861406ed",
+    "url": "/static/media/natours.5f301abf.png"
+  },
+  {
+    "revision": "f434bfbbffc44588572e13e1e113391a",
+    "url": "/static/media/nodejs.f434bfbb.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "cf63ceb8d3e2fed6dcef14d5b4afee2c",
+    "url": "/static/media/redJovenes.cf63ceb8.png"
+  },
+  {
+    "revision": "470548b4a95cae966d0750d6720e4082",
+    "url": "/static/media/sass.470548b4.svg"
+  },
+  {
+    "revision": "aaaf1ab0c0c13f9ee02ab6e86b05118e",
+    "url": "/static/media/section-info.aaaf1ab0.jpg"
+  },
+  {
+    "revision": "a6ea82c0ee2ffe0cb729ff9686beaaa5",
+    "url": "/static/media/tracker.a6ea82c0.png"
+  }
+]);
