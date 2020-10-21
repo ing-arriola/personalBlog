dadab591b98375efc8e846ead5993b65e963aa4c
ing-arriola <ingeniero.arriola@gmail.com>
2020-10-21 07:01:56 -0600

    contact-form test of set up

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,19 +2,23 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div
-      className="form-container"
-      method="POST"
-      data-netlify="true"
-      data-netlify-recaptcha="true"
-    >
+    <div className="form-container">
       <div className="contact-title">
         <h1>Let me know how can I help you</h1>
         <p className="contac-descirption">
           Thanks for contacting me, I'd be happy to answer your hiring questions
         </p>
       </div>
-      <form className="contact-form">
+      <form
+        name="contact"
+        className="contact-form"
+        action="/contact"
+        method="POST"
+        data-netlify-recaptcha="true"
+        netlify
+        hidden
+      >
+        <input type="hidden" name="form-name" value="contact" />
         <label>Name</label>
         <input
           type="input"
